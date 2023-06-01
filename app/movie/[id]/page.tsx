bf5d0ff3797686253af772dbfda0bd192a569fe9
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-06-01 10:39:58 -0400

    feat: render movies from the database

---
+++ app/movie/[id]/page.tsx
@@ -0,0 +1,85 @@
+import { PrismaClient } from "@prisma/client";
+import ReactMarkdown from "react-markdown";
+
+type Kind = "BONUS" | "MALUS";
+
+type Attribute = {
+  id: string;
+  description: string;
+  kind: Kind;
+};
+
+type Movie = {
+  name: string;
+  attributes: Attribute[];
+};
+
+export default async function MovieWrapper({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  async function getMovie(id: string) {
+    "use server";
+
+    const prisma = new PrismaClient();
+
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        attributes: true,
+      },
+    });
+
+    return movie;
+  }
+
+  if (!id) {
+    return null;
+  }
+
+  const movie = await getMovie(id);
+
+  if (!movie) {
+    return null;
+  }
+
+  return (
+    <main>
+      <h1>{movie.name}</h1>
+
+      <section id="attributes" className="flex flex-row justify-around">
+        <article id="bonuses">
+          <h2>Bonuses</h2>
+          <ul>
+            {movie.attributes
+              .filter((attribute) => attribute.kind === "BONUS")
+              .map((bonus) => {
+                return (
+                  <li key={bonus.id}>
+                    <ReactMarkdown>{bonus.description}</ReactMarkdown>
+                  </li>
+                );
+              })}
+          </ul>
+        </article>
+        <article id="maluses">
+          <h2>Maluses</h2>
+          <ul>
+            {movie.attributes
+              .filter((attribute) => attribute.kind === "MALUS")
+              .map((malus) => {
+                return (
+                  <li key={malus.id}>
+                    <ReactMarkdown>{malus.description}</ReactMarkdown>
+                  </li>
+                );
+              })}
+          </ul>
+        </article>
+      </section>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image'
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const prisma = new PrismaClient();
+
+  const movies = await prisma.movie.findMany();
+
   return (
-    <>
+    <main>
       <h1>Historical Movie Accuracy</h1>
-    </>
-  )
+
+      <ul className="list-disc">
+        {movies.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <a href={`/movie/${movie.id}`}>{movie.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
